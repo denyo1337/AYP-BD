152c6a0f356d04142186ba673fe01f798dd8301f
denyo1337 <63121548+denyo1337@users.noreply.github.com>
2022-04-23 10:53:12 +0200

    seed admin acc

--- AYP-BD/WebAPI/Installers/Seeder.cs
+++ AYP-BD/WebAPI/Installers/Seeder.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Enums;
+﻿using Application.Common;
+using Domain.Enums;
 using Domain.Models;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Identity;
@@ -10,20 +11,24 @@ namespace WebAPI.Installers
     {
         private readonly AypDbContext _context;
         private IPasswordHasher<User> _passwordHasher { get; }
-        public Seeder(AypDbContext context, IPasswordHasher<User> passwordHasher)
+        private IEntityGenerator _entityGenerator { get; }
+        private IConfiguration _configuration;
+        public Seeder(AypDbContext context, IPasswordHasher<User> passwordHasher, IEntityGenerator entityGenerator, IConfiguration configuration)
         {
             _context = context;
             _passwordHasher = passwordHasher;
+            _entityGenerator = entityGenerator;
+            _configuration = configuration;
         }
         public void Seed()
         {
-            if(_context is not null && _context.Database.CanConnect())
+            if (_context is not null && _context.Database.CanConnect())
             {
                 if (_context.Database.IsRelational())
                 {
                     //kc for integration tests
                     var pednimgMigrations = _context.Database.GetPendingMigrations();
-                    if(pednimgMigrations != null)
+                    if (pednimgMigrations != null)
                     {
                         _context.Database.Migrate();
                     }
@@ -31,6 +36,11 @@ namespace WebAPI.Installers
                 if (!_context.Roles.Any())
                 {
                     _context.Roles.AddRange(CreateRoles());
+                    _context.SaveChanges();
+                }
+                if (!_context.Users.Any(x => x.RoleId == (byte)AccountTypes.Admin))
+                {
+                    _context.Users.Add(CreateAdmin());
                     _context.SaveChanges();
                 }
             }
@@ -53,5 +63,23 @@ namespace WebAPI.Installers
             };
             return roles;
         }
+        private User CreateAdmin()
+        {
+            var admin = new User()
+            {
+                Id = _entityGenerator.Generate(),
+                Email = "admin@gmail.com",
+                NickName = "admin",
+                Nationality = "admin",
+                Created = DateTime.Now,
+                IsActive = true,
+                IsBanned = false,
+                LastModified = DateTime.Now,
+                RoleId = (byte)AccountTypes.Admin,
+            };
+            admin.PasswordHash = _passwordHasher.HashPassword(admin, _configuration["DefaultPassword"]);
+            return admin;
+        }
+     }
     }
-}
+
